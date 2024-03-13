e06106efd42a42340e6e522bc120189de2591b6d
binyamT <binyampowerhc@gmail.com>
2024-03-14 01:10:50 +0300

    create responsive navigation panel

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,12 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./dist/*.{html,js}"],
+  content: ['./dist/*.{html,js}'],
   theme: {
     extend: {},
+    fontFamily: {
+      'custom-one': ['Alphabetized Cassette Tapes Thin', 'sans-serif'],
+      // Add other font families if needed
+    },
   },
   plugins: [],
-}
-
+};
